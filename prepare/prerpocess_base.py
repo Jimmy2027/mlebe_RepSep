d4a82ae4a1b1c1279f667edf07450640399d665d
Horea Christian <h.chr@mail.ru>
2018-08-13 11:04:13 +0200

    Working!

--- prepare/prerpocess_base.py
+++ prepare/prerpocess_base.py
@@ -6,7 +6,7 @@ bids_base = '~/ni_data/ofM.dr/bids'
 # Preprocess all of the data:
 generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
-	functional_match={'type':['cbv'],},
+	functional_match={'acquisition':['EPIlowcov'],'type':['cbv'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
 	actual_size=True,
 	functional_registration_method="composite",
@@ -15,7 +15,7 @@ generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	)
 generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
-	functional_match={'type':['bold'],},
+	functional_match={'acquisition':['EPIlowcov'],'type':['bold'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
 	actual_size=True,
 	functional_registration_method="composite",
@@ -34,4 +34,7 @@ legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
 # Create 3D collapsed dataset to speed up repeated evaluations
 manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/generic',
 	'~/ni_data/ofM.dr/preprocessing/generic_collapsed',
+	)
+manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/legacy',
+	'~/ni_data/ofM.dr/preprocessing/legacy_collapsed',
 	)
