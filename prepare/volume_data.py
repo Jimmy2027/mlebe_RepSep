9cba9bd3eca99f291d87c298d65629bba349f81e
Horea Christian <h.chr@mail.ru>
2018-08-16 23:58:32 +0200

    New script for volume data summary generation

---
+++ prepare/volume_data.py
@@ -0,0 +1,55 @@
+from snr import df_threshold_volume ,iter_threshold_volume
+from samri.utilities import bids_autograb
+import nibabel as nib
+import numpy as np
+import pandas as pd
+
+base_df = bids_autograb('~/ni_data/ofM.dr/bids_collapsed')
+base_df = base_df.loc[~base_df['path'].str.endswith('.json')]
+base_df = base_df.loc[base_df['type'].isin(['bold','cbv'])]
+base_df['uID'] = base_df['subject']+'_'+base_df['session']+'_'+base_df['type']
+
+generic_df = bids_autograb('~/ni_data/ofM.dr/preprocessing/generic_collapsed')
+generic_df = generic_df.loc[~generic_df['path'].str.endswith('.json')]
+generic_df = generic_df.loc[generic_df['type'].isin(['bold','cbv'])]
+generic_df['uID'] = generic_df['subject']+'_'+generic_df['session']+'_'+generic_df['type']
+
+legacy_df = bids_autograb('~/ni_data/ofM.dr/preprocessing/legacy_collapsed')
+legacy_df = legacy_df.loc[~legacy_df['path'].str.endswith('.json')]
+legacy_df = legacy_df.loc[legacy_df['type'].isin(['bold','cbv'])]
+legacy_df['uID'] = legacy_df['subject']+'_'+legacy_df['session']+'_'+legacy_df['type']
+
+uids = base_df['uID'].unique()
+generic_df = generic_df.loc[generic_df['uID'].isin(uids)]
+legacy_df = legacy_df.loc[legacy_df['uID'].isin(uids)]
+
+base_df['Processing'] = 'Unprocessed'
+generic_df['Processing'] = 'Generic'
+legacy_df['Processing'] = 'Legacy'
+
+base_files = base_df['path'].tolist()
+voxels = np.array([])
+for base_file in base_files:
+	img = nib.load(base_file)
+	data = img.get_data()
+	voxels = np.append(voxels,data)
+voxels = voxels.flatten()
+threshold = np.percentile(voxels,50)
+df = pd.DataFrame([])
+df_ = df_threshold_volume(base_df,
+	threshold=threshold,
+	threshold_is_percentile=False,
+	)
+df = df.append(df_)
+df_ = df_threshold_volume(generic_df,
+	threshold=threshold,
+	threshold_is_percentile=False,
+	)
+df = df.append(df_)
+df_ = df_threshold_volume(legacy_df,
+	threshold=threshold,
+	threshold_is_percentile=False,
+	)
+df_['thresholded volume'] = df_['thresholded volume']/1000.
+df = df.append(df_)
+df.to_csv('vol.csv')
