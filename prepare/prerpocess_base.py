8901034e3421a7dd54bf3d244c4665bd8da5b930
Horea Christian <h.chr@mail.ru>
2018-10-05 16:22:28 +0200

    Completed processingXtemplate matrix testing
    and adjusted threshold

--- prepare/prerpocess_base.py
+++ prepare/prerpocess_base.py
@@ -4,16 +4,32 @@ from samri.pipelines import manipulations
 bids_base = '~/ni_data/ofM.dr/bids'
 
 # Preprocess all of the data:
+generic(bids_base, "~/ambmc_200micron.nii.gz",
+	registration_mask="~/ambmc_200micron_mask.nii.gz",
+	functional_match={'acquisition':['EPIlowcov'],},
+	structural_match={'acquisition':['TurboRARElowcov'],},
+	actual_size=True,
+        keep_work=True,
+	out_base='~/ni_data/ofM.dr/preprocessing',
+	workflow_name='generic_ambmc',
+	)
+legacy(bids_base, '~/ldsurqec_200micron_masked.nii.gz',
+	functional_match={'acquisition':['EPIlowcov']},
+	out_base='~/ni_data/ofM.dr/preprocessing',
+	workflow_name='legacy_dsurqec',
+	)
 generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
 	functional_match={'acquisition':['EPIlowcov'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
 	actual_size=True,
 	out_base='~/ni_data/ofM.dr/preprocessing',
+	workflow_name='generic',
 	)
 legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
 	functional_match={'acquisition':['EPIlowcov']},
 	out_base='~/ni_data/ofM.dr/preprocessing',
+	workflow_name='legacy',
 	)
 
 # Create 3D collapsed dataset to speed up repeated evaluations
@@ -22,11 +38,19 @@ manipulations.collapse_nifti(bids_base,
 	'~/ni_data/ofM.dr/bids_collapsed',
 	#n_jobs_percentage=0.66,
 	)
+manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/generic_ambmc',
+	'~/ni_data/ofM.dr/preprocessing/generic_ambmc_collapsed',
+	#n_jobs_percentage=0.5,
+	)
+manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/legacy_dsurqec',
+	'~/ni_data/ofM.dr/preprocessing/legacy_dsurqec_collapsed',
+	#n_jobs_percentage=0.5,
+	)
 manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/generic',
 	'~/ni_data/ofM.dr/preprocessing/generic_collapsed',
-	#n_jobs_percentage=0.5,
+	n_jobs_percentage=0.5,
 	)
 manipulations.collapse_nifti('~/ni_data/ofM.dr/preprocessing/legacy',
 	'~/ni_data/ofM.dr/preprocessing/legacy_collapsed',
-	#n_jobs_percentage=0.5,
+	n_jobs_percentage=0.5,
 	)
--- prepare/volume_data.py
+++ prepare/volume_data.py
@@ -14,10 +14,20 @@ generic_df = generic_df.loc[~generic_df['path'].str.endswith('.json')]
 generic_df = generic_df.loc[generic_df['type'].isin(['bold','cbv'])]
 generic_df['uID'] = generic_df['subject']+'_'+generic_df['session']+'_'+generic_df['type']
 
+generic_ambmc_df = bids_autograb('~/ni_data/ofM.dr/preprocessing/generic_ambmc_collapsed')
+generic_ambmc_df = generic_ambmc_df.loc[~generic_ambmc_df['path'].str.endswith('.json')]
+generic_ambmc_df = generic_ambmc_df.loc[generic_ambmc_df['type'].isin(['bold','cbv'])]
+generic_ambmc_df['uID'] = generic_ambmc_df['subject']+'_'+generic_ambmc_df['session']+'_'+generic_ambmc_df['type']
+
 legacy_df = bids_autograb('~/ni_data/ofM.dr/preprocessing/legacy_collapsed')
 legacy_df = legacy_df.loc[~legacy_df['path'].str.endswith('.json')]
 legacy_df = legacy_df.loc[legacy_df['type'].isin(['bold','cbv'])]
 legacy_df['uID'] = legacy_df['subject']+'_'+legacy_df['session']+'_'+legacy_df['type']
+
+legacy_dsurqec_df = bids_autograb('~/ni_data/ofM.dr/preprocessing/legacy_dsurqec_collapsed')
+legacy_dsurqec_df = legacy_dsurqec_df.loc[~legacy_dsurqec_df['path'].str.endswith('.json')]
+legacy_dsurqec_df = legacy_dsurqec_df.loc[legacy_dsurqec_df['type'].isin(['bold','cbv'])]
+legacy_dsurqec_df['uID'] = legacy_dsurqec_df['subject']+'_'+legacy_dsurqec_df['session']+'_'+legacy_dsurqec_df['type']
 
 uids = base_df['uID'].unique()
 generic_df = generic_df.loc[generic_df['uID'].isin(uids)]
@@ -25,18 +35,30 @@ legacy_df = legacy_df.loc[legacy_df['uID'].isin(uids)]
 
 base_df['Processing'] = 'Unprocessed'
 generic_df['Processing'] = 'Generic'
+generic_ambmc_df['Processing'] = 'Generic'
 legacy_df['Processing'] = 'Legacy'
+legacy_dsurqec_df['Processing'] = 'Legacy'
+
+base_df['Template'] = 'Unprocessed'
+generic_df['Template'] = 'Generic'
+generic_ambmc_df['Template'] = 'Legacy'
+legacy_df['Template'] = 'Legacy'
+legacy_dsurqec_df['Template'] = 'Generic'
 
 base_df['threshold'] = ''
 generic_df['threshold'] = ''
+generic_ambmc_df['threshold'] = ''
 legacy_df['threshold'] = ''
+legacy_dsurqec_df['threshold'] = ''
 for uid in uids:
 	img = nib.load(base_df.loc[base_df['uID'] == uid, 'path'].item())
 	data = img.get_data()
-	threshold = np.percentile(data,50)
+	threshold = np.percentile(data,66)
 	base_df.loc[base_df['uID'] == uid, 'threshold'] = threshold
 	generic_df.loc[generic_df['uID'] == uid, 'threshold'] = threshold
+	generic_ambmc_df.loc[generic_ambmc_df['uID'] == uid, 'threshold'] = threshold
 	legacy_df.loc[legacy_df['uID'] == uid, 'threshold'] = threshold
+	legacy_dsurqec_df.loc[legacy_dsurqec_df['uID'] == uid, 'threshold'] = threshold
 
 df = pd.DataFrame([])
 df_ = df_threshold_volume(base_df,
@@ -47,7 +69,16 @@ df_ = df_threshold_volume(generic_df,
 	threshold='threshold',
 	)
 df = df.append(df_)
+df_ = df_threshold_volume(generic_ambmc_df,
+	threshold='threshold',
+	)
+df = df.append(df_)
 df_ = df_threshold_volume(legacy_df,
+	threshold='threshold',
+	)
+df_['thresholded volume'] = df_['thresholded volume']/1000.
+df = df.append(df_)
+df_ = df_threshold_volume(legacy_dsurqec_df,
 	threshold='threshold',
 	)
 df_['thresholded volume'] = df_['thresholded volume']/1000.
