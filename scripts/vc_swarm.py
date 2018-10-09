e2487e2cb566d4479c4686330a571201d7812bb7
Horea Christian <horea.christ@yandex.com>
2018-10-09 23:21:10 +0200

    Added and styled VC plots

---
+++ scripts/vc_swarm.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
+from os import path
+
+volume_path = path.abspath('data/volumes.csv')
+df = pd.read_csv(volume_path)
+df.loc[df['Processing']=='Unprocessed', 'Template'] = ''
+ax = sns.swarmplot(
+	x="Processing",
+	y='thresholded volume',
+	data=df,
+	hue="Template",
+	size=mpl.rcParams['lines.markersize'],
+	#data=df.loc[df['Processing']=='Unprocessed'],
+	)
