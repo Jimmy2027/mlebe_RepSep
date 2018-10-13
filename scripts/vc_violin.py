caf3e08276347630e28d38f992a27d6c0a1f24c0
Horea Christian <horea.christ@yandex.com>
2018-10-13 05:43:31 +0200

    Tracking violin plot

---
+++ scripts/vc_violin.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib as mpl
+from os import path
+from itertools import product
+from vendored.categorical import violinplot
+
+# Style
+palette = ['#80e050','#755575']
+
+volume_path = path.abspath('data/volumes.csv')
+df = pd.read_csv(volume_path)
+
+df['Volume Change Factor']=-1
+uids = df['uID'].unique()
+templates = [i for i in df['Template'].unique() if i != 'Unprocessed']
+processings = [i for i in df['Processing'].unique() if i != 'Unprocessed']
+
+for uid, template, processing in list(product(uids,templates,processings)):
+	reference = df.loc[(df['uID']==uid) & (df['Processing']=='Unprocessed'), 'Thresholded Volume'].item()
+	volume = df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Thresholded Volume'].item()
+	df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Volume Change Factor'] = volume/reference
+
+df.loc[df['Processing']=='Unprocessed', 'Template'] = ''
+ax = violinplot(
+	x="Processing",
+	y='Volume Change Factor',
+	data=df.loc[df['Processing']!='Unprocessed'],
+	hue="Template",
+	saturation=1,
+	split=True,
+	inner='quartile',
+	palette=palette,
+	scale='area',
+	dodge=False,
+	inner_linewidth=1.0,
+	linewidth=mpl.rcParams['grid.linewidth'],
+	linecolor='w',
+	)
