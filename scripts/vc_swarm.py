a54b1407d638550087a424b89589319ee43ac5bb
Horea Christian <horea.christ@yandex.com>
2018-10-13 06:32:41 +0200

    Updated labels

--- scripts/vc_swarm.py
+++ scripts/vc_swarm.py
@@ -11,7 +11,7 @@ palette = ['#80e050','#755575']
 volume_path = path.abspath('data/volumes.csv')
 df = pd.read_csv(volume_path)
 
-df['End Volume']=-1
+df['Volume Change Factor']=-1
 uids = df['uID'].unique()
 templates = [i for i in df['Template'].unique() if i != 'Unprocessed']
 processings = [i for i in df['Processing'].unique() if i != 'Unprocessed']
@@ -19,12 +19,12 @@ processings = [i for i in df['Processing'].unique() if i != 'Unprocessed']
 for uid, template, processing in list(product(uids,templates,processings)):
 	reference = df.loc[(df['uID']==uid) & (df['Processing']=='Unprocessed'), 'Thresholded Volume'].item()
 	volume = df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Thresholded Volume'].item()
-	df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'End Volume'] = volume/reference
+	df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Volume Change Factor'] = volume/reference
 
 df.loc[df['Processing']=='Unprocessed', 'Template'] = ''
 ax = sns.swarmplot(
-	x="Processing",
-	y='End Volume',
+	x='Processing',
+	y='Volume Change Factor',
 	data=df.loc[df['Processing']!='Unprocessed'],
 	hue="Template",
 	size=mpl.rcParams['lines.markersize'],
