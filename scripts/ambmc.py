ee17ac5febc833b77eb94c47b7f38918adf92fed
Horea Christian <horea.christ@yandex.com>
2018-10-15 21:32:56 +0200

    Using packaged file location

--- scripts/ambmc.py
+++ scripts/ambmc.py
@@ -2,16 +2,16 @@ from nilearn.plotting import plot_anat
 from samri.plotting.maps import scaled_plot
 import matplotlib.pyplot as plt
 
-template = '/usr/share/mouse-brain-atlases/ambmc_40micron.nii'
+template = '/usr/share/mouse-brain-atlases/lambmc_40micron.nii'
 
 # we explicitly define a figure, because the one internally defined by nilearn overrides matplotlibrc
 # https://github.com/nilearn/nilearn/issues/1777
 fig = plt.figure()
 scaled_plot(template,
-        cut=(0,0,0,),
-        threshold=200000,
-        cmap='gray',
-        scale=0.2,
-        dim=0.1,
-        fig=fig
-        )
+	cut=(0,0,0,),
+	threshold=200000,
+	cmap='gray',
+	scale=0.2,
+	dim=0.1,
+	fig=fig
+	)
