74df3102890ba9d24e9a92b58cb5aa3f51016bc0
Horea Christian <horea.christ@yandex.com>
2018-10-15 21:33:19 +0200

    Using precomputed factors

--- scripts/vc_swarm.py
+++ scripts/vc_swarm.py
@@ -3,23 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
 from os import path
-from itertools import product
 
 # Style
 palette = ['#80e050','#755575']
 
 volume_path = path.abspath('data/volumes.csv')
 df = pd.read_csv(volume_path)
-
-df['Volume Change Factor']=-1
-uids = df['uID'].unique()
-templates = [i for i in df['Template'].unique() if i != 'Unprocessed']
-processings = [i for i in df['Processing'].unique() if i != 'Unprocessed']
-
-for uid, template, processing in list(product(uids,templates,processings)):
-	reference = df.loc[(df['uID']==uid) & (df['Processing']=='Unprocessed'), 'Thresholded Volume'].item()
-	volume = df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Thresholded Volume'].item()
-	df.loc[(df['uID']==uid) & (df['Processing']==processing) & (df['Template']==template), 'Volume Change Factor'] = volume/reference
 
 df.loc[df['Processing']=='Unprocessed', 'Template'] = ''
 ax = sns.swarmplot(
