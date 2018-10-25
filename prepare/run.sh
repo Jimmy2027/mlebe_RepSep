2ec3802f42c9b5e4ebda32ca210ff3a701923759
Horea Christian <h.chr@mail.ru>
2018-10-25 17:11:09 +0200

    Restructured workflow

--- prepare/run.sh
+++ prepare/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 python make_bids.py || exit 1
-python preprocess_base.py || exit 1
+python preprocess.py || exit 1
+python collapse.py || exit 1
 python volume_data.py || exit 1
