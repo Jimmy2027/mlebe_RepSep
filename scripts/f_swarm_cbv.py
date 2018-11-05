3bea2a000b7f49df3d99b07a2e10cf2665b447a1
Horea Christian <horea.christ@yandex.com>
2018-11-05 20:10:08 +0100

    Test functions for functional data

---
+++ scripts/f_swarm_cbv.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib as mpl
+from os import path
+import seaborn as sns
+from itertools import product
+
+# Style
+palette = ['#80e050','#755575']
+
+data_path = path.abspath('data/functional.csv')
+df = pd.read_csv(data_path)
+
+df = df.loc[df['Session']=='ofM']
+
+ax = sns.swarmplot(
+	x="Processing",
+	y='Mean Significance',
+	data=df.loc[df['Contrast']=='CBV'],
+	hue="Template",
+	#saturation=1,
+	split=True,
+	palette=palette,
+	dodge=False,
+	)
+
+ax.set_title('CBV only')
