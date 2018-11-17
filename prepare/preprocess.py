db98b6a89be7b352573c779d4aa091b0a524dacd
Horea Christian <h.chr@mail.ru>
2018-11-17 23:36:55 +0100

    Using packaged templates

--- prepare/preprocess.py
+++ prepare/preprocess.py
@@ -4,20 +4,23 @@ from samri.pipelines import manipulations
 bids_base = '~/ni_data/ofM.dr/bids'
 
 # Preprocess all of the data:
-generic(bids_base, "~/ambmc_200micron.nii.gz",
-	registration_mask="~/ambmc_200micron_mask.nii.gz",
+generic(bids_base,
+	"/home/chymera/mouse-brain-atlas-0.1.20181111/ambmc_200micron.nii",
+	registration_mask="/usr/share/mouse-brain-atlases/ambmc_200micron_mask.nii",
 	functional_match={'acquisition':['EPIlowcov'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
 	actual_size=True,
 	out_base='~/ni_data/ofM.dr/preprocessing',
 	workflow_name='generic_ambmc',
 	)
-legacy(bids_base, '~/ldsurqec_200micron_masked.nii.gz',
+legacy(bids_base,
+	'/usr/share/mouse-brain-atlases/ldsurqec_200micron_masked.nii',
 	functional_match={'acquisition':['EPIlowcov']},
 	out_base='~/ni_data/ofM.dr/preprocessing',
 	workflow_name='legacy_dsurqec',
 	)
-generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
+generic(bids_base,
+	"/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
 	functional_match={'acquisition':['EPIlowcov'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
@@ -25,7 +28,8 @@ generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
 	out_base='~/ni_data/ofM.dr/preprocessing',
 	workflow_name='generic',
 	)
-legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
+legacy(bids_base,
+	"/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
 	functional_match={'acquisition':['EPIlowcov']},
 	out_base='~/ni_data/ofM.dr/preprocessing',
 	workflow_name='legacy',
