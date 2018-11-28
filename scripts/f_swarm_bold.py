b53547b00d21348631f739af81c62e8f1cc94174
Horea Christian <horea.christ@yandex.com>
2018-11-28 11:14:05 +0100

    Unnested BOLD contrast selection

--- scripts/f_swarm_bold.py
+++ scripts/f_swarm_bold.py
@@ -12,11 +12,12 @@ data_path = path.abspath('data/functional.csv')
 df = pd.read_csv(data_path)
 
 df = df.loc[df['Session']=='ofM']
+df = df.loc[df['Contrast']=='BOLD']
 
 ax = sns.swarmplot(
 	x="Processing",
 	y='Mean Significance',
-	data=df.loc[df['Contrast']=='BOLD'],
+	data=df,
 	hue="Template",
 	#saturation=1,
 	split=True,
