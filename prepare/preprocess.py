bbb9bbde55c7f445897cd6a8bbc14d619d2a784a
Horea Christian <h.chr@mail.ru>
2018-11-29 23:31:49 +0100

    Updated maths and atlas paths

--- prepare/preprocess.py
+++ prepare/preprocess.py
@@ -5,7 +5,7 @@ bids_base = '~/ni_data/ofM.dr/bids'
 
 # Preprocess all of the data:
 generic(bids_base,
-	"/home/chymera/mouse-brain-atlas-0.1.20181111/ambmc_200micron.nii",
+	"/usr/share/mouse-brain-atlases/ambmc_200micron.nii",
 	registration_mask="/usr/share/mouse-brain-atlases/ambmc_200micron_mask.nii",
 	functional_match={'acquisition':['EPIlowcov'],},
 	structural_match={'acquisition':['TurboRARElowcov'],},
