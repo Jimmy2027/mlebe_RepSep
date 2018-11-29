d795eacded12ccd4879b3ed64dde009af897be87
Horea Christian <h.chr@mail.ru>
2018-11-29 23:32:02 +0100

    Streamlined logic

--- prepare/functional_data.py
+++ prepare/functional_data.py
@@ -1,49 +1,17 @@
 import pandas as pd
 
 from samri.report.snr import df_significant_signal
+from samri.report.utilities import df_roi_data
 from samri.utilities import bids_autofind_df, bids_autograb
 
 # Total significance
 masks = {
 	'generic':'/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
-	'generic_ambmc':'/usr/share/mouse-brain-atlases/ambmc_200micron_mask.nii.gz',
-	'legacy':'/usr/share/mouse-brain-atlases/lambmc_200micron_mask.nii.gz',
-	'legacy_dsurqec':'/usr/share/mouse-brain-atlases/ldsurqec_200micron_mask.nii.gz',
+	'generic_ambmc':'/usr/share/mouse-brain-atlases/ambmc_200micron_mask.nii',
+	'legacy':'/usr/share/mouse-brain-atlases/lambmc_200micron_mask.nii',
+	'legacy_dsurqec':'/usr/share/mouse-brain-atlases/ldsurqec_200micron_mask.nii',
 	}
-
-df = pd.DataFrame([])
-for key in masks:
-	in_df = bids_autofind_df('~/ni_data/ofM.dr/l1/{}/'.format(key),
-		path_template='sub-{{subject}}/ses-{{session}}/'\
-			'sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_{{modality}}_pfstat.nii.gz',
-		match_regex='.+sub-(?P<sub>.+)/ses-(?P<ses>.+)/'\
-			'.*?_task-(?P<task>.+)_acq-(?P<acquisition>.+)_run-(?P<run>.+)_(?P<modality>cbv|bold)_pfstat\.nii.gz',
-		)
-	df_ = df_significant_signal(in_df,
-		mask_path=masks[key],
-		)
-	if 'generic' in key:
-		df_['Processing'] = 'Generic'
-	else:
-		df_['Processing'] = 'Legacy'
-	if key in ['generic', 'legacy_dsurqec']:
-		df_['Template'] = 'Generic'
-	elif key in ['legacy', 'generic_ambmc']:
-		df_['Template'] = 'Legacy'
-	df = df.append(df_)
-
-# Create processing and template-independent unique identifiers
-df['uID'] = df['subject']+'_'+df['session']+'_'+df['modality']
-
-# Ready Strings for Printing
-df['modality'] = df['modality'].str.upper()
-df = df.rename(columns={'modality': 'Contrast',})
-df.columns = map(str.title, df.columns)
-
-df.to_csv('../data/functional_total.csv')
-
-# DR significance
-masks = {
+masks_dr = {
 	'generic':'/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii',
 	'generic_ambmc':'/usr/share/mouse-brain-atlases/ambmc_200micron_roi-dr.nii',
 	'legacy':'/usr/share/mouse-brain-atlases/lambmc_200micron_roi-dr.nii',
@@ -61,6 +29,14 @@ for key in masks:
 	df_ = df_significant_signal(in_df,
 		mask_path=masks[key],
 		)
+	df_ = df_significant_signal(df_,
+		mask_path=masks_dr[key],
+		column_string='DR Significance',
+		)
+	df_ = df_roi_data(df_,
+		mask_path=masks_dr[key],
+		column_string='DR t',
+		)
 	if 'generic' in key:
 		df_['Processing'] = 'Generic'
 	else:
@@ -76,7 +52,12 @@ df['uID'] = df['subject']+'_'+df['session']+'_'+df['modality']
 
 # Ready Strings for Printing
 df['modality'] = df['modality'].str.upper()
-df = df.rename(columns={'modality': 'Contrast',})
-df.columns = map(str.title, df.columns)
+df.columins = map(str.title, df.columns)
+df = df.rename(
+	columns={
+		'Mean Dr T':'Mean DR t',
+		'Median Dr T':'Median DR t',
+		'Modality':'Contrast',
+		})
 
-df.to_csv('../data/functional_dr.csv')
+df.to_csv('../data/functional.csv')
