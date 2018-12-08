7e3590661f5390fc0c1178471b1f9dd4031c70f1
Horea Christian <horea.christ@yandex.com>
2018-12-08 16:10:13 +0100

    Better filtering

--- scripts/f_swarm_bold.py
+++ scripts/f_swarm_bold.py
@@ -11,12 +11,14 @@ palette = ['#80e050','#755575']
 data_path = path.abspath('data/functional.csv')
 df = pd.read_csv(data_path)
 
-df = df.loc[df['Session']=='ofM']
+#df = df.loc[df['Session']=='ofM']
+
+#df = df.loc[df['Contrast']=='CBV']
 
 ax = sns.swarmplot(
 	x="Processing",
 	y='Mean Significance',
-	data=df.loc[df['Contrast']=='BOLD'],
+	data=df,
 	hue="Template",
 	#saturation=1,
 	split=True,
