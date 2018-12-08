99515785d9ee8b67b2edd3aca17cf06c509acbcc
Horea Christian <h.chr@mail.ru>
2018-12-08 17:50:34 +0100

    Separating measurement type per file

--- prepare/functional_data.py
+++ prepare/functional_data.py
@@ -27,11 +27,41 @@ for key in masks:
 			'.*?_task-(?P<task>.+)_acq-(?P<acquisition>.+)_run-(?P<run>.+)_(?P<modality>cbv|bold)_pfstat\.nii.gz',
 		)
 	df_ = df_significant_signal(in_df,
-		mask_path=masks[key],
-		)
-	df_ = df_significant_signal(df_,
 		mask_path=masks_dr[key],
 		column_string='DR Significance',
+		)
+	if 'generic' in key:
+		df_['Processing'] = 'Generic'
+	else:
+		df_['Processing'] = 'Legacy'
+	if key in ['generic', 'legacy_dsurqec']:
+		df_['Template'] = 'Generic'
+	elif key in ['legacy', 'generic_ambmc']:
+		df_['Template'] = 'Legacy'
+	df = df.append(df_)
+
+# Create processing and template-independent unique identifiers
+df['uID'] = df['subject']+'_'+df['session']+'_'+df['modality']
+
+# Ready Strings for Printing
+df['modality'] = df['modality'].str.upper()
+df.columns = map(str.title, df.columns)
+df = df.rename(
+	columns={
+		'Mean Dr Significance':'Mean DR Significance',
+		'Median Dr Significance':'Median DR Significance',
+		'Modality':'Contrast',
+		})
+
+df.to_csv('../data/functional_significance.csv')
+
+df = pd.DataFrame([])
+for key in masks:
+	in_df = bids_autofind_df('~/ni_data/ofM.dr/l1/{}/'.format(key),
+		path_template='sub-{{subject}}/ses-{{session}}/'\
+			'sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_{{modality}}_tstat.nii.gz',
+		match_regex='.+sub-(?P<sub>.+)/ses-(?P<ses>.+)/'\
+			'.*?_task-(?P<task>.+)_acq-(?P<acquisition>.+)_run-(?P<run>.+)_(?P<modality>cbv|bold)_tstat\.nii.gz',
 		)
 	df_ = df_roi_data(df_,
 		mask_path=masks_dr[key],
@@ -57,9 +87,7 @@ df = df.rename(
 	columns={
 		'Mean Dr T':'Mean DR t',
 		'Median Dr T':'Median DR t',
-		'Mean Dr Significance':'Mean DR Significance',
-		'Median Dr Significance':'Median DR Significance',
 		'Modality':'Contrast',
 		})
 
-df.to_csv('../data/functional.csv')
+df.to_csv('../data/functional_t.csv')
