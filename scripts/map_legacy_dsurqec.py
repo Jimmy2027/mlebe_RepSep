7fe8a6e2047d4dc2dd03384ca7d1b4847a4cfb29
Horea Christian <horea.christ@yandex.com>
2018-12-23 23:01:26 +0100

    Plutting general effect maps

--- scripts/map_legacy_dsurqec.py
+++ scripts/map_legacy_dsurqec.py
@@ -1,17 +1,17 @@
 import samri.plotting.maps as maps
 from os import path
 
-stat_map = '/home/chymera/ni_data/ofM.dr/l2/legacy_dsurqec/ses-ofM/acq-EPIlowcov_run-1_tstat.nii.gz'
+stat_map = '/home/chymera/ni_data/ofM.dr/l2/legacy_dsurqec/acq-EPIlowcov_run-1_tstat.nii.gz'
 template = '/usr/share/mouse-brain-atlases/ldsurqec_40micron_masked.nii'
 maps.stat(stat_maps=[stat_map],
-        template=template,
-        cut_coords=[(0,-4.5,-2.9)],
-        annotate=True,
-        scale=0.5,
-        show_plot=False,
-        interpolation=None,
-        draw_colorbar=True,
-        black_bg=False,
-        threshold=3,
-        dim=0.2,
-        )
+	template=template,
+	#cut_coords=[(0,-4.5,-2.9)],
+	annotate=True,
+	scale=0.2,
+	show_plot=False,
+	interpolation=None,
+	draw_colorbar=True,
+	black_bg=False,
+	threshold=2,
+	dim=0.2,
+	)
