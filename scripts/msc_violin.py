11eaddc6a110b58971470df0f13c44468c4076cf
Horea Christian <horea.christ@yandex.com>
2019-01-02 10:03:13 +0100

    Tracking figures

---
+++ scripts/msc_violin.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib as mpl
+from os import path
+from itertools import product
+from lib.categorical import violinplot
+
+# Style
+palette = ['#ffb66d','#009093']
+
+data_path = path.abspath('data/functional_significance.csv')
+df = pd.read_csv(data_path)
+
+#df = df.loc[~df['Subject'].isin([4003,4006,4013])]
+df = df.loc[df['Processing']!='Unprocessed']
+df = df.loc[((df['Processing']=='Legacy') & (df['Template']=='Legacy')) | ((df['Processing']=='Generic') & (df['Template']=='Generic'))]
+
+df.loc[df['Processing']=='Unprocessed', 'Template'] = ''
+ax = violinplot(
+	x="Processing",
+	y='Mean Significance',
+	data=df,
+	hue="Contrast",
+	saturation=1,
+	split=True,
+	inner='quartile',
+	palette=palette,
+	scale='area',
+	dodge=False,
+	inner_linewidth=1.0,
+	linewidth=mpl.rcParams['grid.linewidth'],
+	linecolor='w',
+	#bw=0.2,
+	)
