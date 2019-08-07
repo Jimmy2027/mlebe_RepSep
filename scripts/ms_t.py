c7f2776861be74dc89a73125e27eb4e74dd90f1b
Horea Christian <horea.christ@yandex.com>
2019-08-07 18:46:52 +0200

    Removed filters

--- scripts/ms_t.py
+++ scripts/ms_t.py
@@ -6,10 +6,6 @@ from lib.utils import float_to_tex
 volume_path = path.abspath('data/functional_significance.csv')
 df = pd.read_csv(volume_path)
 
-#df = df.loc[~df['Subject'].isin([4003,4009,4002,4004,4006])]
-#df = df.loc[~df['Subject'].isin([4003,4009,4002,4004])]
-#df = df.loc[~df['Subject'].isin([4003,4006,4013])]
-
 summary =  ttest_rel(
 	df.loc[(df['Processing']=='Legacy') & (df['Template']=='Legacy'), 'Mean Significance'].tolist(),
 	df.loc[(df['Processing']=='Generic') & (df['Template']=='Generic'), 'Mean Significance'].tolist(),
