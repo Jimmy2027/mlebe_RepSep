1d919db485081c9541d408f7dfe5ec55f954d62b
klugh <klugh@student.ethz.ch>
2020-09-18 10:35:08 +0200

    added masking test

---
+++ tests/test_masking.py
@@ -0,0 +1,28 @@
+import json
+import os
+import shutil
+
+import nibabel as nib
+import numpy as np
+
+import mlebe
+from mlebe.masking.predict_mask import predict_mask
+from mlebe.training.configs.utils import json_to_dict
+
+
+def test_masker():
+    test_dir = os.path.join(os.path.dirname(mlebe.__file__), 'masking/tests/temp/')
+    os.makedirs(test_dir, exist_ok=True)
+    test_config = json_to_dict('../prepare/configs/noBiascorr_noCrop.json')
+    with open(os.path.join(test_dir, 'test_config.json'), 'w') as jsonfile:
+        json.dump(test_config, jsonfile, indent=4)
+    test_in_file = np.ones((63, 96, 48))
+    test_in_file = nib.Nifti1Image(test_in_file, np.eye(4))
+    test_in_file_dir = os.path.join(test_dir, 'test_in_file.nii.gz')
+    nib.save(test_in_file, test_in_file_dir)
+    _, _, _ = predict_mask(test_in_file_dir, os.path.join(test_dir, 'test_config.json'))
+    shutil.rmtree(test_dir)
+
+
+if __name__ == '__name__':
+    test_masker()
