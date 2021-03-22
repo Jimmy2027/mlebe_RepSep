aa45989e4750670de3ccbf09bfce1e8068320271
klugh <klugh@student.ethz.ch>
2021-03-22 10:11:27 +0100

    removed reading of dice score from exp_df

--- prepare/make_config.py
+++ prepare/make_config.py
@@ -10,46 +10,36 @@ import pandas as pd
 from mlebe.training.configs.utils import json_to_dict, write_to_jsonfile, get_dice_score_of_model
 from mlebe.training.utils.utils import mkdir
 
-from classifier.utils import init_model_configs
 from pathlib import Path
 
 # choose your workflow configuration
 JSON_CONFIG_PATH = Path(__file__).parent / "configs/noBiascorr_noCrop.json"
 EXPERIMENT_RESULTS_DF_PATH = os.path.expanduser('~/src/MLEBE/mlebe/training/results.csv')
-SCRATCH_DIR = '~/.scratch/mlebe'
+SCRATCH_DIR = Path('~/.scratch/mlebe').expanduser()
 CONFIG_PATH = os.path.expanduser(os.path.join(SCRATCH_DIR, 'config.json'))
 
 
-def prepare_config(json_config_path: str, scratch_dir: str, experiment_results_df_path: str):
+def prepare_config(json_config_path: Path, scratch_dir: Path, experiment_results_df_path: str):
     mkdir(os.path.expanduser(scratch_dir))
     config = json_to_dict(json_config_path)
-    # overwrite parameters of the model configs such as use_cuda, etc... for the workflow
-    init_model_configs(config)
     # copy the json configuration file to the scratch directory
-    new_config_path = os.path.expanduser(os.path.join(scratch_dir, 'config.json'))
+    new_config_path = scratch_dir / 'config.json'
     verify_config_path(new_config_path)
     copyfile(json_config_path, new_config_path)
     # prepare the workflow config file
     workflow_uid = uuid.uuid4().hex
-    anat_model_config_path = config['masking_config']['masking_config_anat'][
-                                 'model_folder_path'] + '/trained_mlebe_config_anat.json'
-    func_model_config_path = config['masking_config']['masking_config_func'][
-                                 'model_folder_path'] + '/trained_mlebe_config_func.json'
+
     # write workflow uid and model dice scores to new workflow config
-    write_to_jsonfile(new_config_path, [('workflow_config.uid', workflow_uid),
-                                        ('masking_config.masking_config_anat.dice_score',
-                                         get_dice_score_of_model(anat_model_config_path, experiment_results_df_path)),
-                                        ('masking_config.masking_config_func.dice_score',
-                                         get_dice_score_of_model(func_model_config_path, experiment_results_df_path))])
+    write_to_jsonfile(new_config_path, [('workflow_config.uid', workflow_uid)])
     return config, workflow_uid
 
 
 def prepare_experiment_result_dataframe(config: dict, workflow_uid: str):
     # prepare the experiment result dataframe
     parameters = {'with_FLASH': config['workflow_config']['with_FLASH'], 'date': str(datetime.date.today()),
-                  '3D_prediction': True, 'masking_with_bias_corr_anat-func': '{}/ {}'.format(
-            config['masking_config']['masking_config_anat']['bias_correct_bool'],
-            config['masking_config']['masking_config_func']['bias_correct_bool']),
+                  'masking_with_bias_corr_anat-func': '{}/ {}'.format(
+                      config['masking_config']['masking_config_anat']['bias_correct_bool'],
+                      config['masking_config']['masking_config_func']['bias_correct_bool']),
                   'masking_with_cropping': '{}/ {}'.format(
                       config['masking_config']['masking_config_anat']['with_bids_cropping'],
                       config['masking_config']['masking_config_func']['with_bids_cropping']), 'uid': workflow_uid}
