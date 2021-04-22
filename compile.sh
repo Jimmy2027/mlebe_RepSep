3a628f28caf9116eb4768b0ee6e5d771aa1624e9
klugh <klugh@student.ethz.ch>
2021-04-22 10:30:12 +0200

    added compile_article.sh

--- compile.sh
+++ compile.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/env bash
 
-TARGET="${2}"
-SCRATCH_DIR="${1}"
-
+TARGET="${1}"
 WHITELIST="
-	article_ieeetm.tex
+	pitch.tex
+	article.tex
+	poster.tex
+	slides.tex
 	"
-if [["$SCRATCH_DIR" == ""]]; then
-  scratch_dir=~/.scratch/mlebe_threed
-else
-   scratch_dir=~/.scratch/${SCRATCH_DIR}
-fi
-# get all the necessary data from the preprocessing
-#sh prepare/transfer.sh -s ${scratch_dir} -d data || exit
 
-if [[ $TARGET = "all" ]] || [[ "$TARGET" == "" ]]; then
+if [[ "$TARGET" = "all" ]] || [[ "$TARGET" == "" ]]; then
 	for ITER_TARGET in *.tex; do
 		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
 			ITER_TARGET=${ITER_TARGET%".tex"}
-			./compile.sh ${ITER_TARGET}
+			./compile.sh "${ITER_TARGET}"
 		fi
 	done
 else
-	pdflatex -shell-escape ${TARGET}.tex &&\
-	pythontex.py ${TARGET}.tex &&\
-	pdflatex -shell-escape ${TARGET}.tex &&\
-#	bibtex ${TARGET} &&\
-	biber ${TARGET} &&\
-	pdflatex -shell-escape ${TARGET}.tex &&\
-	pdflatex -shell-escape ${TARGET}.tex
-fi
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "Initial pdflatex failed"; exit $ERRCODE; }
+	pythontex "${TARGET}.tex" || { echo "PythonTeX failed"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after PythonTeX"; exit $ERRCODE; }
+	bibtex "${TARGET}" || { echo "bibtex failed"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after bibtex"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex"
+	if [ ! -d logs ]; then
+	  mkdir logs
+	fi
 
-rm auto_fig_py*
-cp "${TARGET}.pdf" $scratch_dir
+	for CLEAN_TARGET in "*.aux" "*.log" "*.out" "*.bbl" "*.pytxcode" "*blx.bib" "*.blg" "*.run.xml"; do
+	  mv $CLEAN_TARGET logs/
+  done
+fi
---
+++ compile_article.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+TARGET="${2}"
+SCRATCH_DIR="${1}"
+
+WHITELIST="
+	article_ieeetm.tex
+	"
+if [["$SCRATCH_DIR" == ""]]; then
+  scratch_dir=~/.scratch/mlebe_threed
+else
+   scratch_dir=~/.scratch/${SCRATCH_DIR}
+fi
+# get all the necessary data from the preprocessing
+#sh prepare/transfer.sh -s ${scratch_dir} -d data || exit
+
+if [[ $TARGET = "all" ]] || [[ "$TARGET" == "" ]]; then
+	for ITER_TARGET in *.tex; do
+		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
+			ITER_TARGET=${ITER_TARGET%".tex"}
+			./compile.sh ${ITER_TARGET}
+		fi
+	done
+else
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pythontex.py ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+#	bibtex ${TARGET} &&\
+	biber ${TARGET} &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex
+fi
+
+rm auto_fig_py*
+cp "${TARGET}.pdf" $scratch_dir
