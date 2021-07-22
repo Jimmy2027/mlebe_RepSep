84da8ec17b33a4991b0a2231a7ff192cc3b3b073
klugh <klugh@student.ethz.ch>
2021-07-22 08:10:31 +0200

    cleanup

--- prepare/run.sh
+++ prepare/run.sh
@@ -5,10 +5,6 @@
 # - mouse-brain-atlases installed
 # need to have run python setup.py develop --user for MLEBE package
 
-#source ~/miniconda3/etc/profile.d/conda.sh
-#conda activate mlebe
-#which python
-
 if [ ! -d ~/.scratch ]; then
   echo "You seem to be lacking a ~/.scratch/ directory."
   echo "We need this directory in order to process the data, and it needs to be on a volume with 200GB+ space."
@@ -17,17 +13,14 @@ if [ ! -d ~/.scratch ]; then
   exit 1
 fi
 
-#ln -s /home/hendrik/.scratch/mlebe_final/classifiers /home/hendrik/.scratch/mlebe/classifiers
-#mkdir -p ~/.scratch/mlebe/preprocessing/generic
-#ln -s ~/.scratch/chymera/irsabi/preprocessing/generic/* ~/.scratch/mlebe/preprocessing/generic/
-
 # This workflow runs with a json configuration file, choose one in configs/ and define it in make_config.py
-if [ ! -f ~/.scratch/mlebe/preprocessing/config.json ]; then
+if [ ! -f ~/.scratch/mlebe/config.json ]; then
   python make_config.py || exit 1
 fi
 
 # Write your workflow description here
 echo "" > ~/.scratch/mlebe/description.txt
+
 python make_bids.py || exit 1
 python preprocess.py || exit 1
 python collapse.py || exit 1
@@ -46,3 +39,4 @@ python classifier/build_graph.py || exit 1
 mv ~/.scratch/mlebe/preprocessing/masked_work/graph.dot ~/.scratch/mlebe/data/masked_nipype.dot || exit 1
 mv ~/.scratch/mlebe/preprocessing/generic_work/graph.dot ~/.scratch/mlebe/data/generic_nipype.dot || exit 1
 sh transfer.sh -s ~/.scratch/mlebe -d ../data || exit 1
+
